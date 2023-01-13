796205e5928045cef3b99693ec07f0369c6c45aa
VikasPushkar1 <vikaspushkar9354@gmail.com>
2023-01-13 14:14:20 +0530

    polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,21 +4,23 @@ import java.util.Scanner;
 
 public class Main {
     public static class Product{
-        public int Product(int x, int y){
+        public int product(int x, int y){
             return  x*y;
         }
-        public int Product(int x, int y, int z){
+        public int product(int x, int y, int z){
             return x*y*z;
         }
-        public double Product(double x, double y){
+        public double product(double x, double y){
             return x*y;
         }
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         Product p=new Product();
-        System.out.println(p.Product(sc.nextInt(),sc.nextInt()));
-        System.out.println(p.Product(sc.nextInt(),sc.nextInt(),sc.nextInt()));
-        System.out.println(p.Product(sc.nextDouble(),sc.nextDouble()));
+
+
+        System.out.println(p.product(sc.nextInt(),sc.nextInt()));
+        System.out.println(p.product(sc.nextInt(),sc.nextInt(),sc.nextInt()));
+        System.out.println(p.product(sc.nextDouble(),sc.nextDouble()));
     }
 }
